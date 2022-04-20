ca6fdca90a694d429423923e03fd312ecc6a2869
Henrick Amaral <henrick.vaz.amaral@gmail.com>
2022-04-20 16:30:10 -0300

    melhoramentos na pagina de criacao de projeto

--- src/componets/paginas/novoProjeto.js
+++ src/componets/paginas/novoProjeto.js
@@ -1,8 +1,11 @@
-
+import styles from './novoProjeto.module.css'
 
 export default function novoProjeto(){
     return (
-        <h1> Novo Projeto </h1>
+        <div className={styles.novoProjeto_container}>
+            <h1>Criando um novo projeto</h1>
+            <p>A partir de agora você irá criar um novo projeto para poder organizar o controlar o andamento.</p>
+        </div>
     )
 }
 
